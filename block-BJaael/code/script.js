d4836552fb3e3e1f2dc9b97afe7e2c262166ea16
swathichowdary123 <swathichowdary548.com>
2023-12-03 10:02:47 +0530

    complete assignment on disco grid

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,23 +1,30 @@
-let container=document.getElementById('bocContainer');
-for(let i=0;i<9;i++){
-    let box=document.createElement('div');
-    box.className='box';
-    box.textContent=generateRandomNumber();
-    box.addEventListener('mouseover',()=>changeColorAndNumber(box));
-    container.appendChild(box);
+function generateRandomNumber(max){
+    return Math.floor(Math.random()*max);
 }
-function changeColorAndNumber(element){
-    let randomColor=generateRandomColor();
-    element.textContent=randomNumber;
-}
-function getRandomColor(){
-    let letters='0123456789abcdef';
+function generateRandomColor(){
+    let hexCharacters=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f",];
     let color='#';
     for(let i=0;i<6;i++){
-        color+=letters[Math.floor(Math.random()*16)];
+        let randomNumber=getrandomNumber(16);
+        color=color+hexCharacters[randomNumber];
     }
     return color;
 }
-function getRandomColor(){
-    return Math.floor(Math.random()*501);
+let parentBoxes=document.querySelector(".boxes");
+for(let i=0;i<500;i++){
+    let div=document.createElement("div");
+    div.classList.add("box");
+    let h3=document.createElement("h3");
+    let randomNo=getRandomNumber(500);
+    h3.innerText=randomNo;
+    div.append(h3);
+    parentBoxes.append(div);
 }
+let allBoxes=document.querySelector(".box");
+function handleMouseMove(){
+    allBoxes.forEach((box)=>{
+        box.style.backgroundColor=generateRandomColor();
+        box.children[0].innerText=getRandomNumber(500);
+    });
+}
+parentBox.addEvenListener("mousemove",handleMouseMove);
