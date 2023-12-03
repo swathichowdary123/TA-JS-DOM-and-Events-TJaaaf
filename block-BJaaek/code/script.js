c6e49091285d141113c1fee3e5097d32473b2395
swathichowdary123 <swathichowdary548.com>
2023-12-03 07:51:06 +0530

    complete assignment on random color generator

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,16 @@
+function changeColor(element){
+    let randomColor=getRandomColor();
+    element.style.backgroundColor=randomColor;
+}
+function changeColorOnMove(element){
+    let randomColor=getRandomColor();
+    element.style.backgroundColor=randomColor;
+}
+function getRandomColor(){
+    let letters='0123456789ABCDEF';
+    let color='#';
+    for(let i=0;i<6;i++){
+        color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
